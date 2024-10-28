99d80ea949b47871e9fbcf6cd6bfde7b11fada01
NgunyiGachie <antogachie@gmail.com>
2024-10-28 23:24:37 +0300

    add simulation

---
+++ data_simulation/simulate_data.py
@@ -0,0 +1,30 @@
+"""
+Simulate data
+"""
+import json
+import random
+import time
+import kafka
+from kafka import KafkaProducer
+
+"""Configure kafka producer"""
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+"""function to generate random energy consumption data"""
+def generate_data(sensor_id):
+    return{
+        "sensor_id": sensor_id,
+        "timestamp": int(time.time()),
+        "energy_consumption": round(random.uniform(100.0, 500.0), 2)
+    }
+
+def main():
+    sensor_id = 1
+    while True:
+        energy_data = generate_data(sensor_id)
+        producer.send('energy_consumption_topic', json.dumps(energy_data).encode('utf-8'))
+        print(f"sent data: {energy_data}")
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
